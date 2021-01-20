60035267f1a242143cafad266e1c3b4af47a19e3
Ankitamagar08 <magarankita25@gmail.com>
2021-01-20 02:03:57 -0800

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using API.Interfaces;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -48,7 +49,10 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
             if (user == null) return Unauthorized("Invalid username");
             using  var hmac = new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
@@ -61,7 +65,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- Instalive/src/app/_services/members.service.ts
+++ Instalive/src/app/_services/members.service.ts
@@ -40,4 +40,13 @@ export class MembersService {
         })
       )
     }
+
+    setMainPhoto(photoId: number){
+      return this.http.put(this.baseUrl + 'User/set-main-photo/'+ photoId, {});
+    }
+
+    deletePhoto(photoId: number)
+    {
+      return this.http.delete(this.baseUrl + 'User/delete-photo/' + photoId);
+    }
 }
